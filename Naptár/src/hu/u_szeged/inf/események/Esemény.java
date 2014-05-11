104ca3d4a2fb49f997603865eed18255757ea351
Kiss Dániel <danielkiss21@gmail.com>
2014-05-11 22:46:47 +0200

    Minimális szerkezet, hogy el lehessen kezdeni a munkát

--- Naptár/src/hu/u_szeged/inf/események/Esemény.java
+++ Naptár/src/hu/u_szeged/inf/események/Esemény.java
@@ -1,5 +1,48 @@
 package hu.u_szeged.inf.események;
 
+import java.util.Calendar;
+
 public class Esemény {
+	
+	private Calendar kezdet;
+	private int időtartam;
+	private String név;
+	
+	public Esemény(String név, Calendar kezdet, int időtartam){
+		
+		this.név = név;
+		this.kezdet = kezdet;
+		this.időtartam = időtartam;
+	}
+
+	public Calendar getKezdet() {
+		return kezdet;
+	}
+
+	public int getIdőtartam() {
+		return időtartam;
+	}
+
+	public String getNév() {
+		return név;
+	}
+
+	public void setKezdet(Calendar kezdet) {
+		this.kezdet = kezdet;
+	}
+
+	public void setIdőtartam(int időtartam) {
+		this.időtartam = időtartam;
+	}
+
+	public void setNév(String név) {
+		this.név = név;
+	}
+	
+	public String toString(){
+		return getNév();
+	}
+	
+	
 
 }
