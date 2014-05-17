a09f92885698e9615a612868de088394284720cf
Kiss Dániel <danielkiss21@gmail.com>
2014-05-17 22:46:14 +0200

    Az alapadatok, azaz a minden eseményhez tartozó adatok mostantól beállíthatók.

--- Naptár/src/UjMunkahelyPanel.java
+++ Naptár/src/UjMunkahelyPanel.java
@@ -1,4 +1,5 @@
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.LayoutManager;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -12,20 +13,27 @@ import javax.swing.SpinnerNumberModel;
 
 public class UjMunkahelyPanel extends JPanel {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1733438848681116239L;
 	private MainFrame frame;
 	private GridBagConstraints c;
 	
 	private JLabel cimkeNev;
 	private JLabel cimkeLeiras;
 	private JLabel cimkeDatum;
+	private JLabel cimkeIdotartam;
 	private JTextField nev;
 	private JTextField leiras;
 	private JSpinner ev;
 	private JSpinner honap;
 	private JSpinner nap;
+	private JSpinner idotartam;
 
 	public UjMunkahelyPanel(MainFrame frame) {
 		this.frame = frame;
+		this.setLayout(new GridBagLayout());
 		c = new GridBagConstraints();
 		c.gridx=0;
 		c.gridy=0;
@@ -50,7 +58,7 @@ public class UjMunkahelyPanel extends JPanel {
 		this.add(cimkeLeiras, c);
 		c.gridx=1;
 		leiras = new JTextField(50);
-		this.add(cimkeLeiras, c);
+		this.add(leiras, c);
 		
 		c.gridx=0;
 		c.gridy=2;
@@ -59,12 +67,23 @@ public class UjMunkahelyPanel extends JPanel {
 		c.gridx=1;
 		ev = new JSpinner(new SpinnerNumberModel(most.get(Calendar.YEAR), most.get(Calendar.YEAR), most.get(Calendar.YEAR)+5, 1));
 		this.add(ev, c);
-		c.gridx=1;
+		c.gridx=2;
 		honap = new JSpinner(new SpinnerNumberModel(most.get(Calendar.MONTH), 1, 12, 1));
 		this.add(honap, c);
-		c.gridx=2;
+		c.gridx=3;
 		nap = new JSpinner(new SpinnerNumberModel(most.get(Calendar.DAY_OF_WEEK), 1, 31, 1));
 		this.add(nap, c);
+		
+		c.gridy=3;
+		c.gridx=0;
+		cimkeIdotartam = new JLabel("Időtartam:");
+		this.add(cimkeIdotartam, c);
+		c.gridx=1;
+		idotartam = new JSpinner(new SpinnerNumberModel(1, 1, 24, 1));
+		this.add(idotartam, c);
+		
+		c.gridy=4;
+		c.gridx=0;
 		
 		
 		
